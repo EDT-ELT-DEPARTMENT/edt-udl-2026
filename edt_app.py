495972b6b6750b39533537dba915e74a608f4d9f
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-01-23 08:04:35 +0100

    Update edt_app.py

--- edt_app.py
+++ edt_app.py
@@ -1011,9 +1011,7 @@ if df is not None:
                 cols_to_drop = [c for c in ['Date_Tri'] if c in df_u_surv.columns]
                 df_u_surv.drop(columns=cols_to_drop).to_excel(buf, index=False)
                 st.download_button(f"📥 Télécharger l'EDT de {prof_sel}", buf.getvalue(), f"Surv_{prof_sel}.xlsx")              
-                    elif portail == "🤖 Générateur Automatique": # Aligné avec le menu principal
-        # --- CET ALIGNEMENT EST CRUCIAL ---
-        
+                      
     elif portail == "🤖 Générateur Automatique":
             if not is_admin:
                 st.error("Accès réservé au Bureau des Examens.")
@@ -1159,6 +1157,7 @@ if df is not None:
         p_etu = st.selectbox("Choisir votre Promotion :", sorted(df["Promotion"].unique()))
         disp_etu = df[df["Promotion"] == p_etu][['Enseignements', 'Code', 'Enseignants', 'Horaire', 'Jours', 'Lieu']]
         st.table(disp_etu.sort_values(by=["Jours", "Horaire"]))
+
 
 
 
