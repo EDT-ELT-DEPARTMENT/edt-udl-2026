ec398a16580330495d765179bf7f6f1dce751887
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-02-03 03:33:10 +0100

    Update edt_app.py

--- edt_app.py
+++ edt_app.py
@@ -737,8 +737,7 @@ with tab_t6:
             st.warning("Aucune donnée disponible pour cet enseignant.")
     else:
         st.warning("Veuillez saisir le code '2026'.")
-
-elif is_admin and mode_view == "Promotion":
+    elif is_admin and mode_view == "Promotion":
     st.subheader("📋 Vue par Promotion")
     
     # Récupération des promotions uniques et triées
@@ -1107,3 +1106,4 @@ elif portail == "🎓 Portail Étudiants":
                 df[cols_format].to_excel(NOM_FICHIER_FIXE, index=False)
                 st.success("✅ Modifications enregistrées !"); st.rerun()
             except Exception as e: st.error(f"Erreur : {e}")
+
